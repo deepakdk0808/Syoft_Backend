5ecfa9e26caaa14fca3d059bbcd3656e291469af
dirtdk <95955656+dirtdk@users.noreply.github.com>
2022-08-02 18:07:02 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -8,16 +8,20 @@ const{register,login}=require("./controllers/auth.controller")
 const app = express()
 app.use(express.json())
 
+const cors=require("cors")
+app.use(cors())
+const port=process.env.PORT ||9875
+
 app.post('/register',register)
 app.post('/login',login)
 
 app.use("/users",userController)
 app.use('/product',productController)
 
-app.listen(9876, async function () {
+app.listen(port, async function () {
     try {
         await connect()
-        console.log('listening on port 9876')
+        console.log(`listening on port ${port}`)
     } catch (error) {
         console.log('error:', error)
     }
